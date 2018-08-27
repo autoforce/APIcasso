f6380d036e3f25808b0b21728db87e8fa1c92fe0
Fernando Bellincanta <ervalhous@hotmail.com>
2018-08-27 14:19:42 -0300

    Adding a little more CORS permissions

--- app/controllers/apicasso/application_controller.rb
+++ app/controllers/apicasso/application_controller.rb
@@ -102,8 +102,9 @@ module Apicasso
     end
 
     def set_access_control_headers
-      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Origin'] = request.headers["Origin"]
       response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+      response.headers['Access-Control-Allow-Credentials'] = 'true'
       response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
       response.headers['Access-Control-Max-Age'] = '1728000'
     end
