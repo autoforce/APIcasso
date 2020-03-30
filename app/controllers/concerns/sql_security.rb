2e96af72145fa0fd140d1ecb982ad3cc3cca2b02
Emanuel Campos <emanuelcampos.contato@gmail.com>
2020-03-30 19:32:23 -0300

    fix verificacao

--- app/controllers/concerns/sql_security.rb
+++ app/controllers/concerns/sql_security.rb
@@ -62,7 +62,9 @@ module SqlSecurity
   # Check for SQL injection before requests to avoid unsafe
   # contantizes and evals along the execution chain
   def bad_request?
-    raise ActionController::BadRequest.new("Bad hacker, stop bullying or I'll tell your mom!") if sql_injection(resource)
+    if if params[:sort] != 'rand'
+      raise ActionController::BadRequest.new("Bad hacker, stop bullying or I'll tell your mom!") if sql_injection(resource)
+    end
   end
 
   # Check for requests using classes outside the application scope,
